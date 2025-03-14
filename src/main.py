c31e3af88652eb2cb3f7813646fa699c83b40429
jpsdias <joaopsdias16@gmail.com>
2025-03-14 00:03:21 +0000

    added withdraw and menu

--- src/main.py
+++ src/main.py
@@ -27,7 +27,6 @@ def create_account(accounts_db):
     new_account = account(id, username)
     accounts_db.append(new_account)
     account.extract(accounts_db[id-1])
-    return accounts_db
 
 def deposit(accounts_db):
     amount = input('Introduce an amount: ')
@@ -41,15 +40,71 @@ def deposit(accounts_db):
         account.extract(accounts_db[0])
     except ValueError:
         print("Invalid input! Please enter a numeric value.")
-    
+
+    return accounts_db
+
+def withdraw(accounts_db):
+    amount = input('Introduce an amount: ')
+
+    try:
+        # Convert the input to a float
+        famount = float(amount)
+        # Round the number to 2 decimal places
+        famount = round(famount, 2)
+
+        account.withdraw(accounts_db[0], famount)
+        account.extract(accounts_db[0])
+    except ValueError:
+        print("Invalid input! Please enter a numeric value.")
+
+def menu(accounts_db):
+    while True:
+        print ("""Select an operation:
+1 - Create an account
+2 - Deposit
+3 - Withdraw
+4 - Transfer
+5 - Consultation
+6 - Exit\n""")
+        
+        value = input("Choose an operation: ")
+        try:
+            ivalue = int(value)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        match ivalue:
+            case 1:
+                create_account(accounts_db)
+            case 2:
+                deposit(accounts_db)
+            case 3:
+                return 'Option three'
+            case 4:
+                return 'Option four'
+            case 5:
+                return 'Option five'
+            case 6:
+                return 0
+            case _:
+                print("Invalid input. Please enter a number.")
 
 def main():
     accounts_db = []
-    print ('Banking App')
+    print("---- Welcome to John's Bank ----")
+    while (True):
+        exit_code = menu(accounts_db)
+        if exit_code == 0:
+            break
 
-    accounts_db = create_account(accounts_db)
+    
 
-    deposit(accounts_db)
+    # accounts_db = create_account(accounts_db)
+
+    # accounts_db = deposit(accounts_db)
+
+    # withdraw(accounts_db)
 
 
 if __name__ == "__main__":
