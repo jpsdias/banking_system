2ac5e680511a0477fcac9c86cfaf4cfb2f8654c3
jpsdias <joaopsdias16@gmail.com>
2025-03-26 00:10:19 +0000

    start test transfer

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,16 @@
 import pytest
+from source.classes import account
 from source.functions import transfer
 
-# def test_transfer():
+def setup_method(self, method):
+    print(f"Setting up {method}")
+    self.id = 1
+    self.username = "João"
+    self.balance = 0.0
+
+@pytest.mark.parametrize("amount", ["random string"])
+def test_string_amount_transfer(self, amount, capsys):
+    transfer(self)
+    captured = capsys.readouterr().out
+    expected_output = ("Error:"    )
+    assert expected_output in captured
