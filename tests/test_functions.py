5eaac5acb3811a10cbeb8d0478e470bd21db7912
jpsdias <joaopsdias16@gmail.com>
2025-03-31 23:29:50 +0100

    added test for id_verification

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,16 +1,68 @@
 import pytest
 from source.classes import account
-from source.functions import transfer
+from source.functions import transfer, id_verification
+from source.exceptions import InsufficientFundsException, NoIdException
 
-def setup_method(self, method):
-    print(f"Setting up {method}")
-    self.id = 1
-    self.username = "João"
-    self.balance = 0.0
+@pytest.fixture
+def src():
+    account_obj = account(1, "T1")
+    account_obj.balance = 100.0
+    return account_obj
+
+@pytest.fixture
+def dest():
+    account_obj = account(2, "T2")
+    account_obj.balance = 20.0
+    return account_obj
+
+@pytest.fixture
+def accounts_db(src, dest):
+    return [src, dest]
 
 @pytest.mark.parametrize("amount", ["random string"])
-def test_string_amount_transfer(self, amount, capsys):
-    transfer(self)
+def test_string_amount_transfer(monkeypatch, src, dest, amount, capsys):
+    monkeypatch.setattr("builtins.input", lambda prompt: amount)
+    
+    transfer(src, dest)
+    
     captured = capsys.readouterr().out
     expected_output = ("Error:"    )
     assert expected_output in captured
+
+@pytest.mark.parametrize("amount", ["200"])
+def test_Inssuf_funds_transfer(monkeypatch, src, dest, amount, capsys):
+    monkeypatch.setattr("builtins.input", lambda prompt: amount)
+    
+    transfer(src, dest)
+    
+    captured = capsys.readouterr().out
+    expected_output = ("Error:"    )
+    assert expected_output in captured
+
+
+@pytest.mark.parametrize("amount", ["50"])
+def test_transfer(monkeypatch, src, dest, amount, capsys):
+    monkeypatch.setattr("builtins.input", lambda prompt: amount)
+    
+    transfer(src, dest)
+    
+    captured = capsys.readouterr().out
+    expected_output = ("----- Account Data -----")
+    assert expected_output in captured
+
+@pytest.mark.parametrize("ids", ["teste", -3])
+def test_string_verification(monkeypatch, accounts_db, ids):
+    monkeypatch.setattr("builtins.input", lambda prompt: ids)
+    with pytest.raises(ValueError):
+        id_verification(accounts_db)
+
+@pytest.mark.parametrize("ids", [3])
+def test_no_id_verification(monkeypatch, accounts_db, ids):
+    monkeypatch.setattr("builtins.input", lambda prompt: ids)
+    with pytest.raises(NoIdException):
+        id_verification(accounts_db)
+
+@pytest.mark.parametrize("ids", [1, 2])
+def test_no_id_verification(monkeypatch, accounts_db, ids):
+    monkeypatch.setattr("builtins.input", lambda prompt: ids)
+    assert id_verification(accounts_db) == ids-1
